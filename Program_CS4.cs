eaa1d92f175427009a1d0502f18a9f2d4464b06c
emilianolsotomayor-hash <emiliano.lsotomayor@gmail.com>
2026-09-24 23:09:21 -0700

    Add files via upload

---
+++ Program_CS4.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CS4 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Sesión 7 : Estructras selectivas
+            // a. Simple: Instrucción if
+            int a = 0;
+            int b = 1;
+            
+            if (a == 0) // si el valor de a es igual a 1 entonces ...
+            {
+                // Bloque de instrucciones.
+                Console.WriteLine("El valor de a es iguañ a 0");
+                a += 1;
+                Console.WriteLine($"a: {a}");
+
+            }
+            // b. Doble : Instrucciones if-else
+            // c. Multiple: Instrucciones if-else if-else 
+        }
+    }
+}

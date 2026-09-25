454db2025b20a05f4879ed0ff6c927eb84a95fcc
emilianolsotomayor-hash <emiliano.lsotomayor@gmail.com>
2026-09-24 22:54:02 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+// Espacio de nombres
+namespace CS1
+{
+    // Clase principal
+    class Program
+    {
+        // Función principál
+        static void Main(string[] args)
+        {
+            //Sesión 4: Introducción a visual studio code.
+            Console.WriteLine("¡Programación en C#!");
+        } // Fin de la función principal
+    } // Término de la clase principal
+} // Término del espacio de nombres
